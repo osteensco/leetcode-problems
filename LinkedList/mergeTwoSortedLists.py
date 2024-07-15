bba5de7ceff82ea0c1b6368e9f39656fbfdb1f18
br0steen <osteensco@gmail.com>
2024-07-15 13:53:55 -0500

    merge linked lists

--- LinkedList/mergeTwoSortedLists.py
+++ LinkedList/mergeTwoSortedLists.py
@@ -34,6 +34,7 @@
 
 
 # Definition for singly-linked list.
+# solution if linked list is ordered
 class ListNode:
     def __init__(self, val=0, next=None):
         self.val = val
@@ -79,4 +80,74 @@ class Solution:
 
         return output
 
+
+
+
+
+
+
+
+
+# Solution for O(1) memory
+
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def reorderList(self, head: Optional[ListNode]) -> None:
+        """
+        Do not return anything, modify head in-place instead.
+        """
+        node = head
+        firsth, lasth = node, node.next
+        
+        while lasth and lasth.next:
+            firsth = firsth.next
+            lasth = lasth.next.next
+        
+        second = firsth.next
+        # break off last node in first half of list
+        firsth.next = None
+        prev = None
+        
+        while second:
+            # set the pointer to shift to at end of iteration
+            n = second.next
+            # reverse the next pointer to the previous node
+            second.next = prev
+            # shift prev to current node
+            prev = second
+            # shift current node to original next pointer
+            second = n
             
+
+        node = head
+        node2 = prev
+
+        while node2:
+            # set pointers to shift at end of iteration
+            n1 = node.next
+            n2 = node2.next
+            #             [n1]
+            # node(1) -> node1(2) -> node1(3) -> ...
+            #              [n2]
+            # node2(1) -> node2(2) -> node2(3) -> ...
+
+            #                         [n2]
+            # node(1) -> node2(1) -> node2(2) -> node2(3) -> ...
+            node.next = node2
+            #                         [n1]
+            # node(1) -> node2(1) -> node1(2) -> node1(3) -> ...
+            node2.next = n1
+            # shift pointers
+            node = n1
+            #                                     [n1]
+            # node(1) -> node2(1) -> node1(2) -> node1(3) -> ...
+            node2 = n2
+            #              [n2]
+            # node2(2) -> node2(3) -> ...
+
+        return head
+
